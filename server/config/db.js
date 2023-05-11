4feb6e74c32ddf5547ad11351e9a12032154f8db
gedeonagmas <gedeonagmas2580@gmail.com>
2023-05-11 14:25:59 +0300

    KKKK

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // const uri =
 //   "mongodb+srv://user2:llR5cnBwy2amBzYn@cluster0.rhayeki.mongodb.net/CAR?retryWrites=true&w=majority&replicaSet=test&ssl=true";
 const uri =
-  "mongodb://user2:llR5cnBwy2amBzYn@ac-walbciw-shard-00-00.rhayeki.mongodb.net:27017,ac-walbciw-shard-00-01.rhayeki.mongodb.net:27017,ac-walbciw-shard-00-02.rhayeki.mongodb.net:27017/?ssl=true&replicaSet=atlas-m5db59-shard-0&authSource=admin&retryWrites=true&w=majority";
+  "mongodb://user2:llR5cnBwy2amBzYn@ac-walbciw-shard-00-00.rhayeki.mongodb.net:27017,ac-walbciw-shard-00-01.rhayeki.mongodb.net:27017,ac-walbciw-shard-00-02.rhayeki.mongodb.net:27017/CAR?ssl=true&replicaSet=atlas-m5db59-shard-0&authSource=admin&retryWrites=true&w=majority";
 async function mongodb() {
   await mongoose.connect(uri, { useNewUrlParser: true });
   console.log("___________database connected___________");
